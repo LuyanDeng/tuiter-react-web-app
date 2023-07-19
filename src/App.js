02fec2ad3c2028a0d94efea9e58c6035e569d91b
Luyan Deng <deng.lu@northeastern.edu>
2023-07-19 16:57:16 -0700

    a3 lab

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
-import logo from './logo.svg';
-import './App.css';
 
+//import './App.css';
+import Labs from "./Labs";
+import HelloWorld from "./Labs/a3/hello-world";
+import Tuiter from "./tuiter";
 function App() {
   return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Welcome to Web Dev SummerII 2023!
-          </p>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div className="container">
+        <HelloWorld/>
+        <Labs/>
+          <Tuiter/>
+        </div>
+
   );
 }
 
 export default App;
+
