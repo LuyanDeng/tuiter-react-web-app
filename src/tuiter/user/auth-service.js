d11cde344ed7dfd0fe15bbcad501e0fd4a9e83f8
petitblue <35588651+petitblue@users.noreply.github.com>
2023-08-15 11:57:30 -0700

    update update user's api

--- src/tuiter/user/auth-service.js
+++ src/tuiter/user/auth-service.js
@@ -2,6 +2,7 @@ import axios from "axios"
 //const SERVER_API_URL = process.env.REACT_APP_SERVER_API_URL; // get the URL to the remote API URL to auth controller
 const SERVER_API_URL = "https://tuiter-node-server-app-a6-stzb.onrender.com/api";
 const USERS_URL = `${SERVER_API_URL}/users`;
+const BASE_API = '${SERVER_API_URL}/api';
 // configure axios to support cookies for passing credentials
 const api = axios.create({withCredentials:true});
 
@@ -25,7 +26,8 @@ export const profile = async () => {
 
 };
 export const updateUser = async (user) => {
-    const response = await api.put(`${USERS_URL}`, user);
+    //const response = await api.put(`${USERS_URL}`, user);
+    const response = await axios.put(`${USER_API}/${id}`, user);
     return response.data;
 };
 export const register = async ({ username, password }) => {
