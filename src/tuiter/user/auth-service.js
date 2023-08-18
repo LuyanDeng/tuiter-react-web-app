5398e40e1c450c84f2b1bd100f1b70b2d6279e6f
petitblue <35588651+petitblue@users.noreply.github.com>
2023-08-18 16:15:33 -0700

    add remote server

--- src/tuiter/user/auth-service.js
+++ src/tuiter/user/auth-service.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 //const SERVER_API_URL = process.env.REACT_APP_SERVER_API_URL; // get the URL to the remote API URL to auth controller
-//const SERVER_API_URL = "https://tuiter-node-server-app-a6-stzb.onrender.com/api";
-const SERVER_API_URL = "http://localhost:4000/api";
+const SERVER_API_URL = "https://tuiter-node-server-app-a6-stzb.onrender.com/api";
+//const SERVER_API_URL = "http://localhost:4000/api";
 const USERS_URL = `${SERVER_API_URL}/users`;
 const BASE_API = '${SERVER_API_URL}/api';
 // configure axios to support cookies for passing credentials
